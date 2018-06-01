83ae0de6642fb1586a36b2dac268bf92c6022112
wezardlza <584121079@qq.com>
2018-06-01 14:02:28 +0100

    Add enumerations.

--- Test_3_Sec_5_7_Sec_5_8/example_class_extra.cpp
+++ Test_3_Sec_5_7_Sec_5_8/example_class_extra.cpp
@@ -4,17 +4,29 @@
 namespace py = pybind11;
 
 struct Pet {
-	Pet(const std::string &name, int age) : name(name), age(age) { }
+	enum Kind {
+		Dog = 0, Cat
+	};
+	Pet(const std::string &name, int age, Kind type) : name(name), age(age), type(type) { }
 	void set(int age_) { age = age_; }
 	void set(const std::string &name_) { name = name_; }
 	std::string name;
+	Kind type;
 	int age;
 };
 
 // 'm' represent the module
 PYBIND11_MODULE(example_class_extra, m) {
-	py::class_<Pet>(m, "Pet")
+	py::class_<Pet> pet(m, "Pet")
 		.def(py::init<const std::string &, int>())
 		.def("set", (void (Pet::*)(int)) &Pet::set, "Set the pet's age")
-		.def("set", (void (Pet::*)(const std::string &)) &Pet::set, "Set the pet's name");
+		.def("set", (void (Pet::*)(const std::string &)) &Pet::set, "Set the pet's name")
+		.def_readwrite("name", &Pet:name)
+		.def_readwrite("age", &Pet::age)
+		.def_readwrite("type", &Pet::type);
+
+	py::enum_<Pet::Kind>(pet, "Kind")
+		.value("Dog", Pet::Kind::Dog)
+		.value("Cat", Pet::Kind::Cat)
+		.export_value();
 }
