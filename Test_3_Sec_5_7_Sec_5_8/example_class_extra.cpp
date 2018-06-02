e93b0737c85016afc27223728e6aba2a7b226a5a
wezardlza <584121079@qq.com>
2018-06-02 17:48:40 +0100

    Test 4 initialized.

--- Test_3_Sec_5_7_Sec_5_8/example_class_extra.cpp
+++ Test_3_Sec_5_7_Sec_5_8/example_class_extra.cpp
@@ -3,11 +3,11 @@
 
 namespace py = pybind11;
 
-struct Pet {
+struct PetN {
 	enum Kind {
 		Dog = 0, Cat
 	};
-	Pet(const std::string &name, int age, Kind type) : name(name), age(age), type(type) { }
+	PetN(const std::string &name, int age, Kind type) : name(name), age(age), type(type) { }
 	void set(int age_) { age = age_; }
 	void set(const std::string &name_) { name = name_; }
 	std::string name;
@@ -17,16 +17,15 @@ struct Pet {
 
 // 'm' represent the module
 PYBIND11_MODULE(example_class_extra, m) {
-	py::class_<Pet> pet(m, "Pet")
-		.def(py::init<const std::string &, int>())
-		.def("set", (void (Pet::*)(int)) &Pet::set, "Set the pet's age")
-		.def("set", (void (Pet::*)(const std::string &)) &Pet::set, "Set the pet's name")
-		.def_readwrite("name", &Pet:name)
-		.def_readwrite("age", &Pet::age)
-		.def_readwrite("type", &Pet::type);
+	py::class_<PetN> pet(m, "PetN"); pet
+		.def(py::init<const std::string &, int, PetN::Kind>())
+		.def("set", (void (PetN::*)(int)) &PetN::set, "Set the pet's age")
+		.def("set", (void (PetN::*)(const std::string &)) &PetN::set, "Set the pet's name")
+		.def_readwrite("name", &PetN::name)
+		.def_readwrite("age", &PetN::age)
+		.def_readwrite("type", &PetN::type);
 
-	py::enum_<Pet::Kind>(pet, "Kind")
-		.value("Dog", Pet::Kind::Dog)
-		.value("Cat", Pet::Kind::Cat)
-		.export_value();
+	py::enum_<PetN::Kind>(pet, "Kind")
+		.value("Dog", PetN::Kind::Dog)
+		.value("Cat", PetN::Kind::Cat);
 }
