e36f6dd70c8303253e120f0faa70d0933444e4ad
wezardlza <584121079@qq.com>
2018-06-04 11:31:41 +0100

    chapter8.cpp finished.

--- Test_4_Sec_8_1_Sec_8_2/chapter8.cpp
+++ Test_4_Sec_8_1_Sec_8_2/chapter8.cpp
@@ -10,22 +10,24 @@ using std::out_of_range;
 C++ Classes
 The pure virtual function should be reloaded with PYBIND11_OVERLOAD_PURE() method;
 PYBIND11_OVERLOAD() should be used for functions which have a default implementation.
-######################################################################################################################*/
+========================================================================================================================
+Module Classes
+----------------------------------------------------------------------------------------------------------------------*/
 
 class Animal {
 public:
 	virtual ~Animal() { }
 	virtual std::string go(int n_times) = 0;
 	virtual std::string name() { return "unknown"; }
-	
-	/*The constructor of the class Animal.*/
-	Animal(double status_percenatge_=100): hp(status_percenatge_){
+
+	// The constructor of the class Animal.
+	Animal(double status_percenatge_ = 100) : hp(status_percenatge_) {
 		if (status_percenatge_ > 100 || status_percenatge_ < 0) {
 			throw out_of_range("The value of the status should be in the domain of [0, 100].");
 		}
 	}
 
-	/*Check the status of the animal based on the hit point range.*/
+	// Check the status of the animal based on the hit point range.
 	std::string check_hp() {
 		if (hp >= 0 && hp <= 30)
 			return "Sleepy.";
@@ -50,7 +52,10 @@ public:
 	}
 	virtual std::string bark() { return "woof!"; }
 };
-/*====================================================================================================================*/
+
+/*======================================================================================================================
+Module Functions
+----------------------------------------------------------------------------------------------------------------------*/
 
 // Dymaic binding: When the function parameter, e.g., 'aminal' below, is a point or reference to a base class, it will
 // trigger a dynamic bindng, under which, the argument can be the base class itself or any class inheriting the base
@@ -62,10 +67,11 @@ std::string call_go(Animal *animal) {
 
 /*######################################################################################################################
 Overloaded C++ Classes
-######################################################################################################################*/
+======================================================================================================================*/
 
 // When combining virtual methods with inheritance, you need to be sure to provide an override for each method
 // for which you want to allow overrides from derived python classes. */
+
 class PyAnimal : public Animal {
 public:
 	/* Inherit the constructors */
@@ -82,8 +88,8 @@ public:
 	using Dog::Dog; // Inherit constructors
 
 	/*------------------------------------------------------------------------------------------------------------------
-	This overloaded function is set with the method of  PYBIND11_OVERLOAD(). Note that the manual in section 8.2, 
-	pp.58 uses PYBIND11_OVERLOAD_PURE() which forces Dog::go as a pure virtual function when *.go() is called from 
+	This overloaded function is set with the method of  PYBIND11_OVERLOAD(). Note that the manual in section 8.2,
+	pp.58 uses PYBIND11_OVERLOAD_PURE() which forces Dog::go as a pure virtual function when *.go() is called from
 	<class 'Dachschund'> which is defined as a child of <class 'Dog'>.
 	------------------------------------------------------------------------------------------------------------------*/
 	std::string go(int n_times) override { PYBIND11_OVERLOAD(std::string, Dog, go, n_times); }
@@ -98,7 +104,7 @@ public:
 
 /*######################################################################################################################
 Registered Python Classes
-######################################################################################################################*/
+======================================================================================================================*/
 
 PYBIND11_MODULE(chapter8, m) {
 	py::class_<Animal, PyAnimal /* <--- trampoline*/>(m, "Animal")
