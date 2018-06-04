af8b2e186bf42678cea96f0f633e062d7cba9642
wezardlza <584121079@qq.com>
2018-06-04 16:53:18 +0100

    chapter8_1 finished.

--- Test_5/chapter8_1.cpp
+++ Test_5/chapter8_1.cpp
@@ -10,22 +10,24 @@ using std::out_of_range;
 C++ Classes
 The pure virtual function should be reloaded with PYBIND11_OVERLOAD_PURE() method;
 PYBIND11_OVERLOAD() should be used for functions which have a default implementation.
-######################################################################################################################*/
+========================================================================================================================
+Module Classes
+----------------------------------------------------------------------------------------------------------------------*/
 
 class Animal {
 public:
 	virtual ~Animal() { }
 	virtual std::string go(int n_times) = 0;
 	virtual std::string name() { return "unknown"; }
-	
-	/*The constructor of the class Animal.*/
-	Animal(double status_percenatge_=100): hp(status_percenatge_){
+
+	// The constructor of the class Animal.
+	Animal(double status_percenatge_ = 100) : hp(status_percenatge_) {
 		if (status_percenatge_ > 100 || status_percenatge_ < 0) {
 			throw out_of_range("The value of the status should be in the domain of [0, 100].");
 		}
 	}
 
-	/*Check the status of the animal based on the hit point range.*/
+	// Check the status of the animal based on the hit point range.
 	std::string check_hp() {
 		if (hp >= 0 && hp <= 30)
 			return "Sleepy.";
@@ -40,23 +42,34 @@ public:
 
 class Dog : public Animal {
 public:
-	Dog(double hp) : Animal(hp) {}
+	
 	Dog() : Animal() {}
+	//A constructor based on a factory function.
+	static Dog create(double status_percentage_ = 100) { return Dog(status_percentage_); }
+
+	virtual std::string bark() { return "woof!"; }
+
 	std::string go(int n_times) override {
 		std::string result;
 		for (int i = 0; i<n_times; ++i)
 			result += bark() + " ";
 		return result;
 	}
-	virtual std::string bark() { return "woof!"; }
+
+private:
+
+	Dog(double hp) : Animal(hp) {}
 };
 
-class Husky : public Dog {};
+class Husky :public Dog {};
 
-/*====================================================================================================================*/
-/*Dymaic binding: When the function parameter, e.g., 'aminal' below, is a point or reference to a base class, it will
-trigger a dynamic bindng, under which, the argument can be the base class itself or any class inheriting the base
-class.*/
+/*======================================================================================================================
+Module Functions
+----------------------------------------------------------------------------------------------------------------------*/
+
+// Dymaic binding: When the function parameter, e.g., 'aminal' below, is a point or reference to a base class, it will
+// trigger a dynamic bindng, under which, the argument can be the base class itself or any class inheriting the base
+// class.
 std::string call_go(Animal *animal) {
 	return animal->go(3);
 }
@@ -64,10 +77,10 @@ std::string call_go(Animal *animal) {
 
 /*######################################################################################################################
 Overloaded C++ Classes
-######################################################################################################################*/
+======================================================================================================================*/
 
-/*When combining virtual methods with inheritance, you need to be sure to provide an override for each method
-for which you want to allow overrides from derived python classes. */
+// When combining virtual methods with inheritance, you need to be sure to provide an override for each method
+// for which you want to allow overrides from derived python classes. */
 
 template <class AnimalBase = Animal> class PyAnimal : public AnimalBase {
 public:
@@ -92,23 +105,35 @@ public:
 
 /*######################################################################################################################
 Registered Python Classes
-######################################################################################################################*/
+======================================================================================================================*/
 
-PYBIND11_MODULE(chapter8, m) {
-	py::class_<Animal, PyAnimal /* <--- trampoline*/>(m, "Animal")
+PYBIND11_MODULE(chapter8_1, m) {
+	// Declare the class of 'Animal'
+	py::class_<Animal, PyAnimal<>> animal(m, "Animal"); 
+	// Declare the class of 'Dog'. Note since the default template argument of 'PyDog' is 'Dog', we can use the below
+	// command instead of "py::class_<Dog, PyDog<Dog>> dog(m, "Dog");"
+	py::class_<Dog, Animal, PyDog<Dog>> dog(m, "Dog");
+	// Declare the class of 'Husky'.
+	py::class_<Husky, Dog, PyDog<Husky>> husky(m, "Husky");
+
+	animal
 		.def(py::init<double>())
 		.def(py::init<>())
 		.def("go", &Animal::go)
 		.def("name", &Animal::name)
 		.def("check_hp", &Animal::check_hp)
 		.def_readwrite("hp", &Animal::hp);
+		
+	dog
+		// The create function can be used as it is or transferred into a constructor.
+		// Both the two definitions can construct an instance of <class 'Dog'>.
+		.def(py::init(&Dog::create)) // a cosntrcutor defined by a factory function
+		.def("create", &Dog::create) // a factory function
 
-	py::class_<Dog, Animal>(m, "Dog")
-		.def(py::init<double>())
 		.def(py::init<>())
 		.def("bark", &Dog::bark);
 
-	py::class_<>
-
+	husky.def(py::init<>());
+		
 	m.def("call_go", &call_go);
 }
